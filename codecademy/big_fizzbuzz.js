e5015ee171fe4f91cb6fdb044d4dca8db1146188
Alejandro Romero <alejo8591@gmail.com>
2012-04-10 17:10:49 -0500

    Push file git_fizzbuzz.js of codeacademy

---
+++ codecademy/big_fizzbuzz.js
@@ -0,0 +1,23 @@
+// Add an else statement in case the number is divisible by 5. 
+
+for (i=1; i<=100; i++) { 
+  if(i % 5 === 0 && i % 3 === 0){
+    console.log("FizzBuzz");
+    continue;
+  }
+  // if the number is divisible by 3, write "Fizz"
+  else if ( i % 3 === 0 && i !==15  && i !==5) { 
+    console.log("Fizz");
+    continue;
+  }
+  
+  // if the number is divisible by 5, write "Buzz"
+  else if (i % 5 === 0 && i !==15 && i !== 3){
+    console.log("Buzz");
+    continue;
+  }
+  // otherwise, write just the number
+  else {
+    console.log(i);
+  }
+}
