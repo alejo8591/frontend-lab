b0ee8436806d533f9003a2556448abb99a8cc199
Alejandro Romero <alejo8591@gmail.com>
2012-07-31 02:18:35 -0500

    update files

---
+++ codecademy/py/unit3.py
@@ -0,0 +1,173 @@
+def clinic():
+    print "You've just entered the clinic!"
+    print "Do you take the door on the left or the right?"
+    answer = raw_input("Type left or right and hit 'Enter'.")
+    if answer == "LEFT" or answer == "Left" or answer == "left":
+        print "This is the Verbal Abuse Room, you heap of parrot droppings!"
+    elif answer == "RIGHT" or answer == "Right" or answer == "right":
+        print "Of course this is the Argument Room, I've told you that already!"
+    else:
+        print "You didn't pick left or right! Try again."
+        clinic()
+
+clinic()
+
+#Assign `True` or `False` as appropriate on the lines below!
+
+# 17 < 118 % 100
+bool_one = True
+
+# 100 == 33 * 3 + 1
+bool_two = True
+
+# 19 <= 2**4
+bool_three =  False
+
+# -22 >= -18
+bool_four = False
+
+#Assign `True` or `False` as appropriate on the lines below!
+
+# 20 + -10 * 2 > 10 % 3 % 2
+bool_one = False
+
+# (10 + 17)**2 == 3**6
+bool_two = True
+
+# 1**2**3 <= -(-(-1))
+bool_three = False
+
+# 40 / 20 * 4 >= -4**2
+bool_four = True
+
+#Create comparative statements as appropriate on the lines below!
+
+#Make me true!
+bool_one = 3 != 2
+
+#Make me false!
+bool_two = 454556 == 123445
+
+#Make me true!
+bool_three = 323435 < 5656787
+
+#Make me false!
+bool_four = 34454.3 >= 1346757876.3
+
+# Assign `True` or `False` as appropriate on the lines below!
+
+# False and False
+bool_one = 3.5 != 3.5 and 3.4 == 3.6
+# -(-(-(-2))) == -2 and 4 >= 16**0.5
+bool_two = False
+# 19 % 4 != 300 / 10 / 10 and False
+bool_three = False
+# -(1**2) < 2**0 and 10 % 10 <= 20 - 10 * 2
+bool_four = True
+# True and True
+bool_five = True
+
+# Assign `True` or `False` as appropriate on the lines below!
+
+# 2**3 == 108 % 100 or 'Cleese' == 'King Arthur'
+bool_one = True or False
+# True or False
+bool_two = 3 < 4 or 23423445 >= 93247143245
+# 100**0.5 >= 50 or False
+bool_three = False or 5 != 5
+# True or True
+bool_four = 2**3 == 108 % 100 or 56 != 123344
+# 1**100 == 100**1 or 3 * 2 * 1 != 3 + 2 + 1
+bool_five = False or False
+
+# Assign `True` or `False` as appropriate on the lines below!
+
+# not True
+bool_one = not 1.25655 < 2.5787
+# not 3**4 < 4**3
+bool_two = True
+# not 10 % 3 <= 10 % 2
+bool_three = True
+# not 3**2 + 4**2 != 5**2
+bool_four = True
+# not not False
+bool_five = False
+
+# Assign `True` or `False` as appropriate on the lines below!
+
+# False or not True and True
+bool_one = 3 >= 4 or 5 != 5 and 34 == 34
+# False and not True or True
+bool_two = 3.4 == 5.2 and not 3 == 3 or 3.5 == 3.5
+# True and not (False or False)
+bool_three = 234 <= 500 and not (500 == 1000 or 10000 != 10000)
+# not not True or False and not True
+bool_four = 5 == 5 or 234 != 234 and 450 >= 100 
+# False or not (True and True)
+bool_five = 5 > 5 or not (56605 >= 400 and 54333 <= 100000)
+
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = 3 == 4 and not True
+
+# Make me true!
+bool_two = 3 >= -2 or 4 <=10
+
+# Make me false!
+bool_three = not 3**4 == 1 and 3*3*3*3**4 <= 0
+
+# Make me true!
+bool_four = not 3**4 == 1 or 3*3*3*3**4 != 0 and 1 <=2282288 
+
+# Make me true!
+bool_five = True
+
+
+#Assign `True` or `False` as appropriate on the lines below!
+
+# 17 < 118 % 100
+bool_one = True
+
+# 100 == 33 * 3 + 1
+bool_two = True
+
+# 19 <= 2**4
+bool_three =  False
+
+# -22 >= -18
+bool_four = False
+
+# 99 != 98 + 1
+bool_five = False
+
+#Assign `True` or `False` as appropriate on the lines below!
+
+# 20 + -10 * 2 > 10 % 3 % 2
+bool_one = False
+
+# (10 + 17)**2 == 3**6
+bool_two = True
+
+# 1**2**3 <= -(-(-1))
+bool_three = False
+
+# 40 / 20 * 4 >= -4**2
+bool_four = True
+
+#Create comparative statements as appropriate on the lines below!
+
+#Make me true!
+bool_one = 3 != 2
+
+#Make me false!
+bool_two = 454556 == 123445
+
+#Make me true!
+bool_three = 323435 < 5656787
+
+#Make me false!
+bool_four = 34454.3 >= 1346757876.3
+
+#Make me true!
+bool_five = 5 <= 10
