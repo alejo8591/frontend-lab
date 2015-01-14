bea416dfe75094b915615111e80566ca4e97db4d
Alejandro Romero <alejo8591@gmail.com>
2015-01-14 01:37:28 -0500

    Install test package: grunt-karma, karma-jasmine, karma-phantomjs-launcher

--- angular/lab10/test/karma.conf.js
+++ angular/lab10/test/karma.conf.js
@@ -26,6 +26,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/jquery-ui/jquery-ui.js',
+      'bower_components/angular-ui-sortable/sortable.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
--- angular/lab10/test/spec/controllers/about.js
+++ angular/lab10/test/spec/controllers/about.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('lab10App'));
+  beforeEach(module('lab10'));
 
   var AboutCtrl,
     scope;
