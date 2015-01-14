df8e4f8348c05fb55f156104548cdfdbec469547
Alejandro Romero <alejo8591@gmail.com>
2015-01-14 15:20:58 -0500

    Update View and Controller for `articles` MVC structure

--- angular/lab11/app/scripts/controllers/articles.js
+++ angular/lab11/app/scripts/controllers/articles.js
@@ -7,11 +7,20 @@
  * # ArticlesCtrl
  * Controller of the lab11App
  */
-angular.module('lab11App')
+angular.module('lab11')
   .controller('ArticlesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.posts = [
+      {
+        title:"Alimentos buenos para la salud",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rhoncus quam leo, id tristique sapien viverra eu. Maecenas ipsum lectus, suscipit auctor tristique in, venenatis ut nisl. Quisque eget bibendum libero. Nam nec mi augue."
+      },
+      {
+        title:"Alimentos menos buenos para la salud",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rhoncus quam leo, id tristique sapien viverra eu. Maecenas ipsum lectus, suscipit auctor tristique in."
+      },
+      {
+        title:"¡¡¡Limita tu cantidad de hidratos de carbono",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rhoncus quam leo, id tristique sapien viverra eu. Maecenas ipsum lectus, suscipit auctor tristique in, venenatis ut nisl. Quisque eget bibendum libero. Nam nec mi augue."
+      }
     ];
   });
