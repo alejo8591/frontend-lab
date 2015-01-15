ebb3dbe0805bd7a36ff1fad917be2cd222a8e663
Alejandro Romero <alejo8591@gmail.com>
2015-01-14 19:15:02 -0500

    Update `members` controller and add AddnewMemberCtrl

--- angular/lab11/app/scripts/controllers/members.js
+++ angular/lab11/app/scripts/controllers/members.js
@@ -8,7 +8,7 @@
  * Controller of the lab11App
  */
 angular.module('lab11')
-.controller('MembersCtrl', function ($scope, $http) {
+.controller('MembersCtrl', function ($scope, $http, $modal) {
   $http.get('http://localhost:9000/members.json')
   .success(function(data){
     $scope.members = data;
@@ -19,6 +19,10 @@ angular.module('lab11')
 
   $scope.gridOptions = {
     data: 'members',
+    showGroupPanel: true,
+    enableCellSelection: true,
+    enableRowSelection: false,
+    enableCellEdit: true,
     columnDefs: [
       {field:'id', displayName:'Nº. Identificación'},
       {field:'name', displayName:'Nombre'},
@@ -26,5 +30,40 @@ angular.module('lab11')
       {field:'registrationDate', displayName:'Fecha de Registro'},
       {field:'memberType', displayName:'Tipo de Miembro'}
     ]
+  };
+
+  $scope.showModal = function(){
+    $scope.newMember = {};
+
+    var modalInstance = $modal.open({
+      templateUrl: 'views/add-members.html',
+      controller: 'AddnewMemberCtrl',
+        resolve: {
+          newMember: function(){
+            return $scope.newMember;
+          }
+        }
+    });
+
+    modalInstance.result.then(function(){
+      $scope.members.push({
+        id: $scope.members.length + 1,
+        name: $scope.newMember.name,
+        registrationDate: $scope.newMember.registrationDate,
+        fidelity: $scope.newMember.fidelity,
+        memberType: $scope.newMember.memberType
+      });
+    });
+  };
+})
+.controller('AddnewMemberCtrl', function($scope, $modalInstance, newMember){
+  $scope.newMember = newMember;
+
+  $scope.saveNewMember = function(){
+    $modalInstance.close(newMember);
+  };
+
+  $scope.cancel = function(){
+    $modalInstance.dismiss('cancel');
   };
 });
