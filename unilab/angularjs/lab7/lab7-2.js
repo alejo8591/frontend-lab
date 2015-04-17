59d80c16411b7b33849d7c9d1563bc7a6046a69c
Alejandro Romero <alejo8591@gmail.com>
2015-04-17 14:40:14 -0500

    Update lab7

--- unilab/angularjs/lab7/lab7-2.js
+++ unilab/angularjs/lab7/lab7-2.js
@@ -1,12 +1,13 @@
-function StudentController($scope){
-  $scope.students = [
-      {"name": "Diego Socja", "phone": "23232323", "course": "Ebsco"},
-      {"name": "Laurao Shot", "phone": "45746547", "course": "Feria"},
-      {"name": "Djuan Piña", "phone": "34543435", "course": "Music"},
-      {"name": "Hgon uYun", "phone": "121888", "course": "Tresco"}
-  ];
+angular.module('Student-App', [])
+       .controller('StudentController', ['$scope', function($scope){
+              $scope.students = [
+                  {"name": "Diego Socja", "phone": "23232323", "course": "Ebsco"},
+                  {"name": "Laurao Shot", "phone": "45746547", "course": "Feria"},
+                  {"name": "Djuan Piña", "phone": "34543435", "course": "Music"},
+                  {"name": "Hgon uYun", "phone": "121888", "course": "Tresco"}
+              ];
 
-   $scope.SaveStudent = function(){
-    $scope.students.push({"name":$scope.newStudent.name, "phone": $scope.newStudent.phone, "course": $scope.newStudent.course});
-  };
-}
+               $scope.SaveStudent = function(){
+                $scope.students.push({"name":$scope.newStudent.name, "phone": $scope.newStudent.phone, "course": $scope.newStudent.course});
+              };
+}]);
--- unilab/angularjs/lab7/lab7-3.js
+++ unilab/angularjs/lab7/lab7-3.js
@@ -1,26 +1,27 @@
-function StudentController($scope){
 
-  $scope.formVisibility = false;
+angular.module('Student-App', [])
+       .controller('StudentController', ['$scope', function($scope){
+              $scope.formVisibility = false;
 
-  $scope.ShowForm = function(){
+              $scope.ShowForm = function(){
 
-    $scope.formVisibility = true;
+                $scope.formVisibility = true;
 
-    console.log($scope.formVisibility);
+                console.log($scope.formVisibility);
 
-  };
+              };
 
-  $scope.students = [
-      {"name": "Diego Socja", "phone": "23232323", "course": "Ebsco"},
-      {"name": "Laurao Shot", "phone": "45746547", "course": "Feria"},
-      {"name": "Djuan Piña", "phone": "34543435", "course": "Music"},
-      {"name": "Hgon uYun", "phone": "121888", "course": "Tresco"}
-  ];
+              $scope.students = [
+                  {"name": "Diego Socja", "phone": "23232323", "course": "Ebsco"},
+                  {"name": "Laurao Shot", "phone": "45746547", "course": "Feria"},
+                  {"name": "Djuan Piña", "phone": "34543435", "course": "Music"},
+                  {"name": "Hgon uYun", "phone": "121888", "course": "Tresco"}
+              ];
 
-   $scope.SaveStudent = function(){
+               $scope.SaveStudent = function(){
 
-    $scope.students.push({"name":$scope.newStudent.name, "phone": $scope.newStudent.phone, "course": $scope.newStudent.course});
+                $scope.students.push({"name":$scope.newStudent.name, "phone": $scope.newStudent.phone, "course": $scope.newStudent.course});
 
-    $scope.formVisibility = false;
-  };
-}
+                $scope.formVisibility = false;
+              };
+}]);
