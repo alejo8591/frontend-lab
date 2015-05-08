df8a0ce63a24a26c62da3d87df03c4cc6fbd69a1
Alejandro Romero <alejo8591@gmail.com>
2015-05-08 13:02:03 -0500

    Update lab5

--- unilab/cor/lab5/www/js/lab5.js
+++ unilab/cor/lab5/www/js/lab5.js
@@ -25,16 +25,16 @@ function onHeadingError(compassError){
 	if (compassError.code == CompassError.COMPASS_NOT_SUPPORTED) {
 
 		data_node.innerHTML = '<b>Bruluja no disponible</b>';
-		alert('<b>Bruluja no disponible</b>');
+		alert('Bruluja no disponible');
 
 	} else if (compassError.code == CompassError.COMPASS_INTERNAL_ERR) {
 
 		data_node.innerHTML = '<b>Compass Internal Error</b>';
-		alert('<b>Compass Internal Error</b>');
+		alert('Compass Internal Error');
 
 	} else {
 
 		data_node.innerHTML = '<b>Error indeterminado o no reconocido por el API</b>';
-		alert('<b>Error indeterminado o no reconocido por el API</b>');
+		alert('Error indeterminado o no reconocido por el API');
 	}
 }
