4869c0faaae06acb3bcf65a89a1cbbe196b258fa
Alejandro Romero <alejandro.romero@dmkcolomiba.com>
2015-06-12 11:58:46 -0500

    Update proda for proda_one, push proda_two

---
+++ unilab/proda_one/www/js/controllers/prodapp.controller.index.js
@@ -0,0 +1,74 @@
+/* http://ow.ly/Od3WF */
+
+'use strict';
+
+angular.module('prodapp.controller.index',
+    [
+        'ionic',
+        'prodapp.cookie.provider'
+    ]
+)
+
+.controller('index', function($scope, $state, cookieProvider) {
+
+        console.log( cookieProvider.flagCookie() );
+
+        if( cookieProvider.flagCookie() ) {
+
+
+        } else {
+
+            $state.transitionTo('options');
+
+        }
+})
+
+
+.controller('options', function($scope, $state, cookieProvider) {
+
+
+    if( cookieProvider.flagCookie() ) {
+
+        $state.transitionTo('index');
+
+    } else {
+
+        $scope.loadLogin = function() {
+
+            $state.transitionTo('login');
+
+        };
+
+        $scope.loadRegister = function() {
+
+            $state.transitionTo('register');
+
+        };
+
+    }
+})
+
+
+.controller('login', function($scope, $state, cookieProvider) {
+
+    if( cookieProvider.flagCookie() ) {
+
+        $state.transitionTo('index');
+
+    } else {
+
+
+    }
+})
+
+.controller('register', function($scope, $state, cookieProvider) {
+
+    if( cookieProvider.flagCookie() ) {
+
+        $state.transitionTo('index');
+
+    } else {
+
+
+    }
+});
