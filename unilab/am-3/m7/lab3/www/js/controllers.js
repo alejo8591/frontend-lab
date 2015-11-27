707729d57fb1c53c0ffdeb942eb7740e27a2dc60
Alejandro Romero <alejandro.romero@dmkcolomiba.com>
2015-11-27 11:53:16 -0500

    Update l3 :squirrel:

--- unilab/am-3/m7/lab3/www/js/controllers.js
+++ unilab/am-3/m7/lab3/www/js/controllers.js
@@ -48,11 +48,42 @@ angular.module('app.controllers', ['ionic'])
 
 })
 
-.controller('modalCtrl', function($scope) {
+.controller('modalCtrl', function($scope, $ionicModal, $timeout){
+
+  $ionicModal.fromTemplateUrl('my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+
+  // For example's sake, hide the sheet after two seconds
+  $timeout(function(){
+    $scope.openModal();
+  }, 1000);
 
 })
 
-.controller('popupCtrl', function($scope) {
+.controller('popupCtrl', function($scope, $ionicPopup, $timeout) {
 
 })
 
@@ -62,4 +93,4 @@ angular.module('app.controllers', ['ionic'])
 
 .controller('homeCtrl', function($scope) {
 
-})
+});
