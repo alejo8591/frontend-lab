ec01cc70de508865abbbfe1d88dbc23aeffcb868
Alejandro Romero <alejo8591@gmail.com>
2016-10-07 16:02:43 -0500

    Update repo

---
+++ unilab/amfour/m7/lab88/www/js/controllers.js
@@ -0,0 +1,63 @@
+angular.module('app.controllers', ['ionic'])
+
+  .controller('actionSheetCtrl', ['$scope', '$stateParams', '$ionicActionSheet', '$timeout', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $ionicActionSheet, $timeout) {
+      // Triggered on a button click, or some other target
+      $scope.show = function() {
+        // Show the action sheet
+        var hideSheet = $ionicActionSheet.show({
+          buttons: [
+            {text: '<b>Share</b> This'},
+            {text: 'Move'}
+          ],
+          destructiveText: 'Delete',
+          titleText: 'Modify your album',
+          cancelText: 'Cancel',
+          cancel: function () {
+            // add cancel code..
+          },
+          buttonClicked: function (index) {
+            return true;
+          }
+        });
+
+        // For example's sake, hide the sheet after two seconds
+        $timeout(function (){
+          hideSheet();
+        }, 4000);
+      }
+    }])
+
+  .controller('modalCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {
+
+
+    }])
+
+  .controller('popupsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {
+
+
+    }])
+
+  .controller('listCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {
+
+
+    }])
+
+  .controller('detailListCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {
+
+
+    }])
