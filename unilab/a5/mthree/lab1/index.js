816d6ba1bd27c8d20fb9b15d0b896ce9794aae1c
Alejandro Romero <alejo8591@gmail.com>
2016-10-21 13:01:30 -0500

    Update apps

--- unilab/a5/mthree/lab1/index.js
+++ unilab/a5/mthree/lab1/index.js
@@ -1,3 +1,4 @@
+'use strict';
 var my_array = [1, 2, 3, 4, 5];
 var other_array = ["array", 3, 5, 6, [1, 2, 3, 4]];
 var tasks = [1, 3, 4, 5];
@@ -5,20 +6,5 @@ function greeter(person) {
     return person;
 }
 var user = ["1", "32"];
-var yopo = ["yopp", "treo"];
-var literla = 'yopo';
-literla = 'trepe';
 console.log(greeter(user));
-var list = ["s", "s", "d"];
-for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
-    var i = list_1[_i];
-    console.log(i);
-}
-var Generic = (function () {
-    function Generic() {
-    }
-    return Generic;
-}());
-var my_gen = new Generic();
-console.log(my_gen.add = function (x, y) { return x + y; });
-console.log(my_gen.add(3, 4));
+var a = 'a';
--- unilab/a5/mthree/lab1/index.ts
+++ unilab/a5/mthree/lab1/index.ts
@@ -1,38 +1,17 @@
-var my_array: Array<number> = [1, 2, 3, 4, 5];
+'use strict';
 
-var other_array: Array<any> = ["array", 3, 5, 6, [1, 2, 3, 4]];
+var my_array: Array<number> = [1,2,3,4,5];
 
-var tasks: Array<number> = [1, 3, 4, 5];
+var other_array: Array<any> = ["array", 3, 5, 6, [1,2,3,4]];
 
-function greeter(person: Array<string>) {
+var tasks: Array<number> = [1,3,4,5];
+
+function greeter(person: Array<string>){
     return person;
 }
 
-var user: Array<string> = ["1", "32"];
-
-var yopo: Array<any> = ["yopp", "treo"];
-
-let literla = 'yopo';
-
-literla = 'trepe';
+var user: Array<string> = ["1","32"];
 
 console.log(greeter(user));
 
-var list = ["s", "s", "d"];
-
-for (let i of list) {
-    console.log(i);
-}
-
-class Generic<T> {
-    add: (X: T, Y: T) => T;
-}
-
-let my_gen = new Generic<number>();
-
-console.log(my_gen.add = function (x, y) { return x + y });
-console.log(my_gen.add(3, 4));
-
-
-
-
+var a: string = 'a';
