40a31598f8fee8fdcd8459ecf213d6a4e80276e9
Alejandro Romero <alejo8591@gmail.com>
2017-08-11 18:27:44 -0500

    Lmac

---
+++ unpick/m22/l66/index.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('.toggle-nav').click(function() {
+        toggleNav();
+    });
+});
+
+function toggleNav() {
+    if ($('#site-wrapper').hasClass('show-nav')) {
+        // Do things on Nav Close
+        $('#site-wrapper').removeClass('show-nav');
+    } else {
+        // Do things on Nav Open
+        $('#site-wrapper').addClass('show-nav');
+    }
+}
